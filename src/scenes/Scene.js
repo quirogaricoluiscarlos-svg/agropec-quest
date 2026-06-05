338f439553a2194569706eacbd3a9ad9d7238e8a
Luis Carlos Quiroga Rico <quirogaricoluiscarlos@gmail.com>
2026-06-05 10:41:40 -0500

    feat: add Scene base class

---
+++ src/scenes/Scene.js
@@ -0,0 +1,10 @@
+// src/scenes/Scene.js
+export class Scene {
+  constructor(engine) {
+    this.engine = engine;
+  }
+  init()        {}
+  update(_dt)   {}
+  draw(_ctx)    {}
+  destroy()     {}
+}
